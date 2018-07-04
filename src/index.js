d9d84c039f908dc84f34c0d60abc537b28914af0
Raj  Kumar Shrestha <rzkmrstha@gmail.com>
2018-07-04 13:59:56 +1000

    set route and views

--- src/index.js
+++ src/index.js
@@ -1,9 +1,59 @@
+/*!
+ * KMVN - index.js
+ * Copyright(c) 2018 rzkmr
+ * MIT Licensed
+ */
+
+'use strict';
+
 import Koa from 'koa';
+import logger from 'koa-logger';
+import render from 'koa-ejs';
+import Router from 'koa-router';
+import path from 'path';
 
 const app = new Koa();
 
-app.use(async ctx => {
-  ctx.body = 'Hello World';
+const PORT = process.env.PORT || 3000;
+const wlecomeMsg = `
+== Welcome to KMVN ==
+   ***************
+`;
+
+app.use(logger());
+
+render(app, {
+  root: path.join(__dirname, 'views'),
+  layout: 'layout',
+  viewExt: 'html',
+  cache: false,
+  debug: true,
 });
 
-app.listen(3000);
+// create routes
+const router = new Router();
+
+router.get('/', async (ctx, next) =>
+  // ctx.body = body.replace('{csrfToken}', ctx.csrf)
+
+  ctx.render('index', {
+    title: 'KMVN page',
+    csrfToken: ctx.csrf,
+  })
+);
+
+// Load routes
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+const server = app.listen(PORT, () => {
+  console.log(`Server listening on  http://localhost:${PORT}`);
+  console.log(wlecomeMsg);
+});
+
+app.on('error', function (err) {
+  console.log(err.stack);
+});
+
+module.exports = server;
